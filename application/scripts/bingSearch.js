dd53c3b2b633aaac7f1af4012bbb153c4e709476
Vuk Petrovic <vukpetrovic1@yahoo.com>
2015-03-28 22:34:30 -0400

    Figured out the bing api, bitches.

--- application/scripts/bingSearch.js
+++ application/scripts/bingSearch.js
@@ -2,13 +2,13 @@
 function searchBing() {
 	var searchBar = document.getElementById("searchBar");
 	var searchQuery = searchBar.value;
-	var searchRoute = "https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27";
+	var searchRoute = "https://user:zYdO5g8rm3yqbGpS4pJXCUTU++qEPBcnm9BfXXeP5DQ@api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27";
 	searchRoute += searchQuery;
-	searchRoute += "%27";
+	searchRoute += "%27&$top=10&$format=JSON";
 
 	var sender = new XMLHttpRequest();
 	sender.onreadystatechange = function() {
-		if(sender.readyState == 4 && sender.status = 200) {
+		if(sender.readyState == 4 && sender.status == 200) {
 			//we're good, the search worked
 			var searchResults = JSON.parse(sender.responseText);
 			console.log(searchResults);
