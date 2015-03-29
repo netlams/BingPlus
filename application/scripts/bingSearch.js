3e0ea45216dc4ff114e3b1046aff360e1472e789
Vuk Petrovic <vukpetrovic1@yahoo.com>
2015-03-29 00:01:28 -0400

    parsed bing api json

--- application/scripts/bingSearch.js
+++ application/scripts/bingSearch.js
@@ -14,7 +14,8 @@ function searchBing() {
 			//we're good, the search worked
 			var searchResults = JSON.parse(sender.responseText);
 			console.log(searchResults);
-			displayResults("This works");
+
+			displayResults(searchResults);
 		} 
 	}
 	sender.open("GET", searchRoute, "true");
@@ -24,4 +25,52 @@ function searchBing() {
 
 function displayResults(results) {
 	//Modify UI (logo fades out) and search bar moves to top
+	var siteList = results.d.results;
+	var siteListContainer = document.createElement("div");
+	siteListContainer.id = "siteListContainer";
+
+	var cap = document.getElementById("cap");
+	cap.insertBefore(siteListContainer, cap.childNodes[0]);
+
+	for(var i = 0; i < siteList.length; i++) {
+		var title = siteList[i].Title;
+		var dURL = siteList[i].DisplayUrl;
+		var desc = siteList[i].Description;
+
+		var itemWrapper = document.createElement("div");
+		itemWrapper.className = "linkwrapper";
+		siteListContainer.appendChild(itemWrapper);
+
+		var titleDisplay = document.createElement("h1");
+		titleDisplay.className = "linktitle"
+		var titleText = document.createTextNode(title);
+		titleDisplay.appendChild(titleText);
+		itemWrapper.appendChild(titleDisplay);
+
+
+		var urlLink = document.createElement("a");
+		urlLink.href = "http://" + dURL;
+		urlLink.rel = "external";
+		itemWrapper.appendChild(urlLink);
+
+		var urlDisplay = document.createElement("h3");
+		urlDisplay.className = "linkurl";
+		var urlText = document.createTextNode(dURL);
+		urlDisplay.appendChild(urlText);
+		urlLink.appendChild(urlDisplay);
+
+		var descDisplay = document.createElement("p");
+		descDisplay.className = "linkdesc";
+		var descText = document.createTextNode(desc);
+		descDisplay.appendChild(descText);
+		itemWrapper.appendChild(descDisplay);
+
+		var likeButton = document.createElement("div");
+		likeButton.className = "likeButton";
+		var commentButton = document.createElement("div");
+		commentButton.className = "commentButton";
+
+		itemWrapper.appendChild(likeButton);
+		itemWrapper.appendChild(commentButton);
+	}
 }
